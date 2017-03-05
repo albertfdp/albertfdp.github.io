fbc9b37d03a0aaa4d66b109c3469e4092026d91c
Albert Fernández <albert@nandez.cat>
2017-03-05 13:27:06 +0100

    Remove deprecation warning

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -52,7 +52,7 @@ module.exports = {
         test: /\.css/,
         loaders: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          loader: 'css-loader?modules&localIdentName=[path][name]--[local]--[hash:base64:5]!postcss-loader'
+          use: 'css-loader?modules&localIdentName=[path][name]--[local]--[hash:base64:5]!postcss-loader'
         }),
         include: paths.source
       },
